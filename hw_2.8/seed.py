a059582667220540a127f0ae1c75a953488aa1a9
szepano <156124248+szepano@users.noreply.github.com>
2024-05-17 15:35:23 +0200

    Add files via upload

---
+++ hw_2.8/seed.py
@@ -0,0 +1,26 @@
+import json
+from models import Tag, Author, Quote
+
+
+def insert_from_json(authors_file, quotes_file):
+
+    with open(authors_file, 'r') as fh:
+        authors = json.load(fh)
+
+    for i in authors:
+        new_auth = Author(name=i['fullname'],
+                        born=i['born_date'],
+                        born_in=i['born_location'],
+                        desc=i['description']
+                        ).save()
+    
+    with open(quotes_file, 'r') as fh:
+        quotes = json.load(fh)
+
+    for i in quotes:
+        tags = Tag(name=(t for t in i['tags']))
+        
+
+
+if __name__ == '__main__':
+    insert_from_json('authors.json', 'quotes.json')
